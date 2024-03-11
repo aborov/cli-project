fc168f890e12f01e3ed923631d7025d691781847
Aleksei Borovikov <161416353+aborov@users.noreply.github.com>
2024-03-11 23:22:11 +0000

    added foir

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Watchlist CLI
-
 A command line interface app for film fans to keep a watchlist of movies to see
+
+## Usage
+Run `ruby app.rb` in the terminal
---
+++ watchlist.rb
@@ -0,0 +1,13 @@
+class Watchlist
+
+  def initialize
+    @title = title
+    @year = year
+  end
+
+  def start
+    puts "\n"
+    puts "Hey movie fan, welcome to the Watchlist CLI!"
+    puts "What you want to do today?"
+    puts "1. Add a film to watch sometime"
+    puts "2. "
